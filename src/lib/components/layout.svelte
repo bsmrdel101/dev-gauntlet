c0038f9eacd30095f9c0c4080646e1e2335d2d67
Bennett Smrdel <bennett@midwestdiesel.com>
2024-04-18 15:20:05 -0500

    feat: Initial commit

---
+++ src/lib/components/layout.svelte
@@ -0,0 +1,7 @@
+<div class="layout">
+  <div class="layout__container">
+    <div class="layout__main-content">
+      <slot />
+    </div>
+  </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Layout from "$lib/components/layout.svelte";
+</script>
+
+<Layout>
+  <h1>Hello World</h1>  
+</Layout>
