786150b6f61386fb69d3565d1e0ec0fdaaf4826f
Bennett Smrdel <bennett@midwestdiesel.com>
2024-04-19 13:49:35 -0500

    feat: add difficulty and platform to challenges

--- src/lib/components/Challenge.svelte
+++ src/lib/components/Challenge.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+  import { capitalize } from "$lib/scripts/tools/utils";
+
   export let challenge: Challenge;
 </script>
 
 
 <a class="challenge" href={`/challenges/${challenge.id}`}>
-  <h2>{ challenge.title }</h2>
+  <h2>{ challenge.title } <span>{ challenge.platform }</span></h2>
+  <h3 class={`challenge__difficulty challenge__difficulty--${challenge.difficulty}`}>{ capitalize(challenge.difficulty) }</h3>
   <img src={challenge.image} alt={challenge.image} loading="lazy" draggable="false" />
   <p class="challenge__desc">{ challenge.desc }</p>
 </a>
