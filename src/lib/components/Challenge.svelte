30a58cb92de4730200f6292671b72854dd0ca711
Bennett Smrdel <bennett@midwestdiesel.com>
2024-04-19 15:43:14 -0500

    feat: Make challenge details page

--- src/lib/components/Challenge.svelte
+++ src/lib/components/Challenge.svelte
@@ -6,8 +6,7 @@
 
 
 <a class="challenge" href={`/challenges/${challenge.id}`}>
-  <h2>{ challenge.title } <span>{ challenge.platform }</span></h2>
+  <h2 class="challenge__title">{ challenge.title } <span>{ challenge.platform }</span></h2>
   <h3 class={`challenge__difficulty challenge__difficulty--${challenge.difficulty}`}>{ capitalize(challenge.difficulty) }</h3>
   <img src={challenge.image} alt={challenge.image} loading="lazy" draggable="false" />
-  <p class="challenge__desc">{ challenge.desc }</p>
 </a>
--- src/routes/challenges/[id]/+page.svelte
+++ src/routes/challenges/[id]/+page.svelte
@@ -2,6 +2,7 @@
   import { page } from '$app/stores';
   import Layout from "$lib/components/Layout.svelte";
   import { api } from '$lib/scripts/config/api';
+  import { capitalize } from '$lib/scripts/tools/utils';
   import { onMount } from 'svelte';
   let { params } = $page;
 
@@ -15,6 +16,16 @@
 
 <Layout title={challenge && challenge.title}>
   {#if challenge}
-    <h1>{ challenge.title }</h1>
+    <h3 class={`challenge__difficulty challenge__difficulty--${challenge.difficulty}`}>{ capitalize(challenge.difficulty) }</h3>
+    <h1 class="challenge__title">{ challenge.title } <span>{ challenge.platform }</span></h1>
+    <img class="challenge__cover-img" src={challenge.image} alt={challenge.image} loading="lazy" draggable="false" />
+    <p class="challenge__desc">{ challenge.desc }</p>
   {/if}
 </Layout>
+
+
+<style lang="scss">
+  h1 {
+    font-size: var(--font-xl);
+  }
+</style>
